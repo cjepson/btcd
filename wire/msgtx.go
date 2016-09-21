organicbitcoin/obtcd

wire: Reduce allocs with contiguous slices.
The current code involves a ton of small allocations which is harsh on
the garbage collector and in turn causes a lot of addition runtime
overhead both in terms of additional memory and processing time.

In order to improve the situation, this drasticially reduces the number
of allocations by creating contiguous slices of objects and
deserializing into them.  Since the final data structures consist of
slices of pointers to the objects, they are constructed by pointing them
into the appropriate offset of the contiguous slice.

This could be improved upon even further by converting all of the data
structures provided the wire package to be slices of contiguous objects
directly, however that would be a major breaking API change and would
end up requiring updating a lot more code in every caller.  I do think
that ultimately the API should be changed, but the changes in this
commit already makes a massive difference and it doesn't require
touching any of the callers, so it is a good place to begin.

The following is a before and after comparison of the allocations
with the benchmarks that did not change removed:

benchmark              old allocs     new allocs     delta
-----------------------------------------------------------
DeserializeTxLarge     16715          11146          -33.32%
DecodeGetHeaders       501            2              -99.60%
DecodeHeaders          2001           2              -99.90%
DecodeGetBlocks        501            2              -99.60%
DecodeAddr             3001           2002           -33.29%
DecodeInv              50003          3              -99.99%
DecodeNotFound         50002          3              -99.99%
DecodeMerkleBlock      107            3              -97.20%

wire/msgtx.go
@@ -263,14 +263,16 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.BtcDecode", str)
 	}
 
+	// Deserialize the inputs.
+	txIns := make([]TxIn, count)
 	msg.TxIn = make([]*TxIn, count)
 	for i := uint64(0); i < count; i++ {
-		ti := TxIn{}
-		err = readTxIn(r, pver, msg.Version, &ti)
+		ti := &txIns[i]
+		err = readTxIn(r, pver, msg.Version, ti)
 		if err != nil {
 			return err
 		}
-		msg.TxIn[i] = &ti
+		msg.TxIn[i] = ti
 	}
 
 	count, err = ReadVarInt(r, pver)
@@ -288,14 +290,16 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.BtcDecode", str)
 	}
 
+	// Deserialize the outputs.
+	txOuts := make([]TxOut, count)
 	msg.TxOut = make([]*TxOut, count)
 	for i := uint64(0); i < count; i++ {
-		to := TxOut{}
-		err = readTxOut(r, pver, msg.Version, &to)
+		to := &txOuts[i]
+		err = readTxOut(r, pver, msg.Version, to)
 		if err != nil {
 			return err
 		}
-		msg.TxOut[i] = &to
+		msg.TxOut[i] = to
 	}
 
 	msg.LockTime, err = binarySerializer.Uint32(r, littleEndian)

cjepson/btcd

wire/msgtx.go
@@ -573,14 +573,15 @@ func (msg *MsgTx) decodePrefix(r io.Reader, pver uint32) error {
 	}
 
 	// TxIns.
+	txIns := make([]TxIn, count)
 	msg.TxIn = make([]*TxIn, count)
 	for i := uint64(0); i < count; i++ {
-		ti := TxIn{}
-		err = readTxInPrefix(r, pver, msg.Version, &ti)
+		ti := &txIns[i]
+		err = readTxInPrefix(r, pver, msg.Version, ti)
 		if err != nil {
 			return err
 		}
-		msg.TxIn[i] = &ti
+		msg.TxIn[i] = ti
 	}
 
 	count, err = ReadVarInt(r, pver)
@@ -599,14 +600,15 @@ func (msg *MsgTx) decodePrefix(r io.Reader, pver uint32) error {
 	}
 
 	// TxOuts.
+	txOuts := make([]TxOut, count)
 	msg.TxOut = make([]*TxOut, count)
 	for i := uint64(0); i < count; i++ {
-		to := TxOut{}
-		err = readTxOut(r, pver, msg.Version, &to)
+		to := &txOuts[i]
+		err = readTxOut(r, pver, msg.Version, to)
 		if err != nil {
 			return err
 		}
-		msg.TxOut[i] = &to
+		msg.TxOut[i] = to
 	}
 
 	// Locktime and expiry.
@@ -642,14 +644,15 @@ func (msg *MsgTx) decodeWitness(r io.Reader, pver uint32, isFull bool) error {
 			return messageError("MsgTx.decodeWitness", str)
 		}
 
+		txIns := make([]TxIn, count)
 		msg.TxIn = make([]*TxIn, count)
 		for i := uint64(0); i < count; i++ {
-			ti := TxIn{}
-			err = readTxInWitness(r, pver, msg.Version, &ti)
+			ti := &txIns[i]
+			err = readTxInWitness(r, pver, msg.Version, ti)
 			if err != nil {
 				return err
 			}
-			msg.TxIn[i] = &ti
+			msg.TxIn[i] = ti
 		}
 		msg.TxOut = make([]*TxOut, 0)
 	} else {
@@ -683,9 +686,10 @@ func (msg *MsgTx) decodeWitness(r io.Reader, pver uint32, isFull bool) error {
 
 		// Read in the witnesses, and copy them into the already generated
 		// by decodePrefix TxIns.
+		txIns := make([]TxIn, count)
 		for i := uint64(0); i < count; i++ {
-			ti := TxIn{}
-			err = readTxInWitness(r, pver, msg.Version, &ti)
+			ti := &txIns[i]
+			err = readTxInWitness(r, pver, msg.Version, ti)
 			if err != nil {
 				return err
 			}
@@ -719,14 +723,15 @@ func (msg *MsgTx) decodeWitnessSigning(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.decodeWitness", str)
 	}
 
+	txIns := make([]TxIn, count)
 	msg.TxIn = make([]*TxIn, count)
 	for i := uint64(0); i < count; i++ {
-		ti := TxIn{}
-		err = readTxInWitnessSigning(r, pver, msg.Version, &ti)
+		ti := &txIns[i]
+		err = readTxInWitnessSigning(r, pver, msg.Version, ti)
 		if err != nil {
 			return err
 		}
-		msg.TxIn[i] = &ti
+		msg.TxIn[i] = ti
 	}
 	msg.TxOut = make([]*TxOut, 0)
 
@@ -752,14 +757,15 @@ func (msg *MsgTx) decodeWitnessValueSigning(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.decodeWitness", str)
 	}
 
+	txIns := make([]TxIn, count)
 	msg.TxIn = make([]*TxIn, count)
 	for i := uint64(0); i < count; i++ {
-		ti := TxIn{}
-		err = readTxInWitnessValueSigning(r, pver, msg.Version, &ti)
+		ti := &txIns[i]
+		err = readTxInWitnessValueSigning(r, pver, msg.Version, ti)
 		if err != nil {
 			return err
 		}
-		msg.TxIn[i] = &ti
+		msg.TxIn[i] = ti
 	}
 	msg.TxOut = make([]*TxOut, 0)
 
@@ -843,14 +849,16 @@ func (msg *MsgTx) LegacyBtcDecode(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.BtcDecode", str)
 	}
 
+	// Deserialize the inputs.
+	txIns := make([]TxIn, count)
 	msg.TxIn = make([]*TxIn, count)
 	for i := uint64(0); i < count; i++ {
-		ti := TxIn{}
-		err = legacyReadTxIn(r, pver, msg.Version, &ti)
+		ti := &txIns[i]
+		err = legacyReadTxIn(r, pver, msg.Version, ti)
 		if err != nil {
 			return err
 		}
-		msg.TxIn[i] = &ti
+		msg.TxIn[i] = ti
 	}
 
 	count, err = ReadVarInt(r, pver)
@@ -868,14 +876,16 @@ func (msg *MsgTx) LegacyBtcDecode(r io.Reader, pver uint32) error {
 		return messageError("MsgTx.BtcDecode", str)
 	}
 
+	// Deserialize the outputs.
+	txOuts := make([]TxOut, count)
 	msg.TxOut = make([]*TxOut, count)
 	for i := uint64(0); i < count; i++ {
-		to := TxOut{}
-		err = legacyReadTxOut(r, pver, msg.Version, &to)
+		to := &txOuts[i]
+		err = legacyReadTxOut(r, pver, msg.Version, to)
 		if err != nil {
 			return err
 		}
-		msg.TxOut[i] = &to
+		msg.TxOut[i] = to
 	}
 
 	msg.LockTime, err = binarySerializer.Uint32(r, littleEndian)